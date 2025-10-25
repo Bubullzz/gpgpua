713670d1a86f4cd596aaa7c5ba0ad265dac3bebc
Bulle <bulle.mostovoi@gmail.com>
2025-10-25 14:56:57 +0200

    not working version but commit before i break my histograms

--- project_irgpua/src/radix_sort.cu
+++ project_irgpua/src/radix_sort.cu
@@ -1,28 +1,104 @@
 #include "radix_sort.cuh"
 
 #define NB_BINS 10
+#define NB_VALUES_PER_BLOCK 4
 
+__device__ int global_hist[NB_BINS];
 
-__global__ void radix_sort_kernel(raft::device_span<int> in, raft::device_span<int> out, int size, int iteration)
+__global__ void init_global_hist(raft::device_span<int> in, int iteration)
 {
-    int tid = threadIdx.x;
-    // Get Blocks Local Histogram
-    __shared__ int shared_hist[NB_BINS * 2 + 1]; // assuming NB_BINS elements
-    int *inc_cum_hist = &shared_hist[NB_BINS + 1]; // inclusive cum hist
-    int *exc_cum_hist = &shared_hist[NB_BINS]; // exclusive cum hist
-
-    if (tid < NB_BINS * 2 + 1)
-        shared_hist[tid] = 0;
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    if (tid < NB_BINS)
+        global_hist[tid] = 0;
     __syncthreads();
 
-    if (tid < size)
+    int pos = tid;
+    int n = in.size();
+    while (pos < n)
     {
-        int value = in[tid];
+        int value = in[pos];
+        for (int i = 0; i < iteration; ++i)
+        {
+            value /= NB_BINS;
+        }
+        atomicAdd(&global_hist[value % NB_BINS], 1);
+        pos += stride;
+    }
+
+    __syncthreads();
+    return;
+
+}
+
+// dummy version for the moment
+__global__ void global_hist_cum_sum(raft::device_span<int> in)
+{
+    if (blockIdx.x == 0 && threadIdx.x == 0)
+    {
+        int last = global_hist[0];
+        global_hist[0] = 0;
+        for (int i = 1; i < NB_BINS; ++i)
+        {
+            int tmp = global_hist[i];
+            global_hist[i] = last + global_hist[i - 1];
+            last = tmp;
+        }
+    }
+    __syncthreads();
+    if (blockIdx.x == 0 && threadIdx.x == 0)
+    {
+        for (int i = 0; i < NB_BINS; ++i)
+        {
+            in[i] = global_hist[i];
+        }
+    }
+    __syncthreads();
+    return;
+}
+
+__global__ void radix_sort_kernel(raft::device_span<int> in,
+    raft::device_span<int> out,
+    int iteration,
+    int* block_num)
+{
+    // Static shared memory allocation
+    __shared__ int shared_hist[NB_BINS * 2 + 1]; // Room for local hist
+    int *inc_cum_hist = &shared_hist[NB_BINS + 1]; // inclusive cum hist
+    int *exc_cum_hist = &shared_hist[NB_BINS]; // exclusive cum hist
+ 
+    cuda::atomic_ref<int, cuda::thread_scope_device> block(*block_num);
+
+    if (threadIdx.x == 0)
+    {
+        my_block_id = block.fetch_add(1);
+        to_add = 0;
+    }
+    __syncthreads();
+
+    int block_base = blockIdx.x * NB_VALUES_PER_BLOCK;
+    int tid = threadIdx.x;
+
+    // Initialize shared memory
+    int offset = tid;
+    while(offset < NB_BINS * 2 + 1)
+    {
+        shared_hist[offset] = 0;
+        offset += blockDim.x;
+    }
+    __syncthreads();
+
+    // Build Local Histogram
+    offset = tid;
+    while (offset < NB_VALUES_PER_BLOCK)
+    {
+        int value = in[block_base + offset];
         for (int i = 0; i < iteration; ++i)
         {
             value /= NB_BINS;
         }
         atomicAdd(&shared_hist[value % NB_BINS], 1);
+        offset += blockDim.x;
     }
     __syncthreads();
 
@@ -91,19 +167,36 @@ void radix_sort(rmm::device_uvector<int>& in, rmm::device_uvector<int>& out, int
 {
     int size = in.size();
 
-    //size_t max_threads = 1024;
-    //size_t thread_per_block = max_threads;
+    size_t max_threads = 1024;
+    size_t thread_per_block = max_threads;
+    size_t nb_blocks = 64;
+    rmm::device_scalar<int> block_num(0, stream);
+
+
+    init_global_hist<<<nb_blocks, thread_per_block, 0, stream>>>(raft::device_span<int>(in.data(), size), 1);
+    global_hist_cum_sum<<<1, 1, 0, stream>>>(raft::device_span<int>(in.data(), size));
+    cudaMemcpyAsync(out.data(), in.data(), NB_BINS * sizeof(int),
+                    cudaMemcpyDeviceToDevice, stream);
+    cudaStreamSynchronize(stream);
+
+
+    size_t all_hists_size = NB_BINS * sizeof(int) * nb_blocks;
+    rmm::device_uvector<int> states(nb_blocks, stream);
+    rmm::device_uvector<int> local_hists(all_hists_size, stream);
+    rmm::device_uvector<int> exclusive_prefix_summed_hists(all_hists_size, stream);
+    return;
+
     //size_t threads = ;
     //size_t blocks = (threads + thread_per_block - 1) / thread_per_block;
     // each thread = 4 pixels and 4 predicate values
     //size_t shared_memory_size = thread_per_block * 4 * 2 * sizeof(int);
     for (int iteration = 0; max_value > 0; iteration++)
     {
+        block_num.set_value(0);
         radix_sort_kernel<<<1, size, 0, stream>>>
             (raft::device_span<int>(in.data(), size),
             raft::device_span<int>(out.data(), size),
-            size, 
-            iteration);
+            iteration, block_num.data());
         
             max_value /= NB_BINS;
         cudaStreamSynchronize(stream);
@@ -116,10 +209,10 @@ void radix_sort(rmm::device_uvector<int>& in, rmm::device_uvector<int>& out, int
 // Just run this function to test perfs and correctness
 bool test_radix_sort()
 {
-    int n = 1024; // nb of values in array
+    int n = 20; // nb of values in array
     std::vector<int> array(n);
     std::mt19937 rng(42);
-    int max = 10000000;
+    int max = 10;
     for (int& x : array)
         x = rng() % max;
 
@@ -152,7 +245,7 @@ bool test_radix_sort()
     cudaMemcpyAsync(gpu_array.data(), my_gpu_d_out.data(), gpu_array.size() * sizeof(int),
                     cudaMemcpyDeviceToHost, my_gpu_stream);
     cudaStreamDestroy(my_gpu_stream);
-    
+
     // ---------------------------
     // CUB reference GPU sort
     // ---------------------------
@@ -208,9 +301,9 @@ bool test_radix_sort()
         std::cout << "✅ GPU radix sort matches both CPU & CUB results!" << std::endl;
     else
     {
-        if (false)
+        std::cout << "❌ GPU sort mismatch!\n";
+        if (true)
         {
-            std::cout << "❌ GPU sort mismatch!\n";
             for (int i = 0; i < n; ++i)
             {
                 if (cpu_array[i] != gpu_array[i] || cub_array[i] != gpu_array[i])
